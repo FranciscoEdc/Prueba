56cd9b4314b8e949feab2c4cd69d6a24d7919ca9
FranciscoEdc <franciscofg22@educastur.es>
2023-11-29 10:21:53 +0100

    ndfkdfjkdfjkdfjkdf

---
+++ JS/prueba.js
@@ -0,0 +1,14 @@
+// Crear nodo tipo párrafo
+var parrafo = document.createElement("p");
+
+// Crear nodo tipo texto
+var contenido = document.createTextNode("Hola otra vez, mundo");
+
+// Añadir el nodo texto al párrafo
+parrafo.appendChild("contenido");
+
+// Añadir el párrafo al documento
+document.body.appendChild("parrafo");
+
+// Eliminar el párrafo
+parrafo.body.removeChild("parrafo")
