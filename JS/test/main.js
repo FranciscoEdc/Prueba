1e4ee06a5e10cddb18d9804ef5dc950214677962
FranciscoEdc <franciscofg22@educastur.es>
2023-12-20 10:19:31 +0100

    sdfjklsdfjklsdfsdfsdf

--- JS/test/main.js
+++ JS/test/main.js
@@ -2,29 +2,34 @@
 function resultado(){
 
     var puntuacion = 0;
-var p1 = document.getElementsByName('a');
-var p2 = document.getElementsByName('b');
-var p3 = document.getElementsByName('c');
-var p4 = document.getElementsByName('d');
-var p5 = document.getElementsByName('e');
+    var p1 = document.getElementsByName('a');
+    var p2 = document.getElementsByName('b');
+    var p3 = document.getElementsByName('c');
+    var p4 = document.getElementsByName('d');
+    var p5 = document.getElementsByName('e');
+    var p6 = document.getElementById('f');
+    var p7 = document.getElementsByName('g');
 
     if (p1[1].checked) {
         puntuacion++;
     }
-    alert("Tu puntuación es " + puntuacion + "/5.");
- /*   if (p2[2].checked("true")) {
-        (parseInt("puntuacion"))+1;
+    if (p2[2].checked) {
+        puntuacion++;
     }
-    if (p3[0].checked("true")) {
-        (parseInt("puntuacion"))+1;
+    if (p3[0].checked) {
+        puntuacion++;
     }
-    if (p4[3].checked("true")) {
-        (parseInt("puntuacion"))+1;
+    if (p4[3].checked) {
+        puntuacion++;
     }
-    if (p5[3].checked("true")) {
-        (parseInt("puntuacion"))+1;
+    if (p5[3].checked) {
+        puntuacion++;
     }
-
-    window.alert(puntuacion);
-   */ 
+    if (p6.value=="GNU's Not Unix") {
+        puntuacion++;
+    }
+    if (p7[0].checked && p7[2].checked && p7[4].checked && p7[5].checked ) {
+        puntuacion++;
+    }
+    alert("Tu puntuación es " + puntuacion + "/7.");
 }
